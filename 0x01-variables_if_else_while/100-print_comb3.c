5508135cb72e2346e03d97e00e366759ab5180bd
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-07 11:19:25 -0700

    l

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 	int ch;
 	int n;
 
-	for (ch = 47; ch <= 56; ch++)
+	for (ch = 48; ch <= 57; ch++)
 {
-	for (n = 48; n <= 56; n++)
+	for (n = 49; n <= 57; n++)
 {
 	if (n > ch)
 {
 	putchar(ch);
 	putchar(n);
-	if (ch != 55 || n != 56)
+	if (ch != 56 || n != 57)
 {
 	putchar(44);
 	putchar(32);

f893e4903093c86a3a37449a32b6263c6d012082
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-07 10:39:07 -0700

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Returns: Always 0 (success)
+ */
+void main(void)
+{
+	int ch;
+
+	for (ch = 48; ch  <= 57; ch++)
+{
+	putchar(ch);
+	if (ch != 57)
+{
+	putchar(44);
+	putchar(32);
+}
+}
+	putchar(10);
+	return (0);
+}

ebc067032a1063284fb4f95a3de34ac9c125ef6a
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-07 03:52:40 -0700

    l

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+/**
+ * main - prints all single digits of base 10 starting from 0
+ * followed by a new line
+ * Retun: Always 0
+ */
+
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 56; ch <= 60; ch++)
+{
+	putchar(ch);
+}
+	putchar(10);
+	return (0);
+}

36f1c5da98e1e2801637444db813a5f6533c3044
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-14 12:11:24 -0700

    l

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,37 +6,23 @@
  */
 
 char *cap_string(char *str)
-
 {
-
 	int i, c;
 
 	int trigger;
 
 	char nots[] = ",;.!?(){}\n\t\" ";
 
-
-
 	for (i = 0, trigger = 0; str[i] != '\0'; i++)
-
 {
-
 	if (str[0] > 96 && str[0] < 123)
-
 	trigger = 1;
-
 	for (c = 0; nots[c] != '\0'; c++)
-
 {
-
 	if (nots[c] == str[i])
-
 	trigger = 1;
-
 }
-
 	if (trigger)
-
 {
 	if (str[i] > 96 && str[i] < 123)
 {

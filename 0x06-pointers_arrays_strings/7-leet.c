22c5410b5be80c63f39f4a17fbefc241d64850f4
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-14 12:01:05 -0700

    l

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -25,17 +25,11 @@ char *leet(char *s)
 	s[i] = '3';
 
 	while (s[i] == 'o' || s[i] == 'O')
-
 	s[i] = '0';
-
 	while (s[i] == 't' || s[i] == 'T')
-
 	s[i] = '7';
-
 	while (s[i] == 'l' || s[i] == 'L')
-
 	s[i] = '1';
-
 }
 	return (s);
 

67c2f23ef91306db4715661735bb4bc65f79fdd6
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-27 04:21:09 -0700

    l

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: int type number
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c > 91)
+		return (1);
+	return (0);
+}

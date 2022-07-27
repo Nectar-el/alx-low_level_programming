54363b8e84a423edfe5c7cd163e238c58d482b02
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-27 09:57:35 -0700

    l

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit -  checks for a digit (0 through 9).
+ * @c: int type number
+ * Return: 1 if c is a digit, otherwise return 0.
+ */
+int _isdigit(int c)
+{
+	if (c > 0 && c < 10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

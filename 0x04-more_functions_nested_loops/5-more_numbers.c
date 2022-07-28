ec3ad51c9639b55583e0bd55dbeb077200d20231
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-28 04:09:51 -0700

    l

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	n = 0;
+	while (n <= 10)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
+

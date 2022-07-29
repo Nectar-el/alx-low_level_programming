27f462e5beb7fbcc13229180a526aa0cccac5241
Ndiritu Eliud Maina <mainaeliud3099@gmail.com>
2022-07-29 03:11:40 -0700

    l

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,13 @@ void more_numbers(void)
 	int n;
 	int i;
 
-	 for (n = 1; n <= 10; n++)
+	for (n = 1; n <= 10; n++)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i > 14)
-			_putchar(i + '0');
+			if (i >= 14)
+			_putchar('1');
+			_putchar(i % 10 + '0');
 		}
 	}
 	_putchar('\n');
